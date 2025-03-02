2af4ef644927b87943cb45602bb64e5764e5c014
Samuel Meier <ssmeier1@gmail.com>
2025-03-02 00:21:02 -0500

    Way too many changes before a commit :(

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 class EtchBoard {
-    constructor(x,y){
-        this.x = x
-        this.y = y
 
-    }
-
-    generateBoard (x,y) {
-        var totalSquares = (x * y);
-        for(let i = 0; i <= totalSquares; i++){
-            let pixel = document.createElement("div");
-            console.log(pixel)
-            pixel.classList.add('pixel');
-            console.log(document.querySelector('.board-container'))
-            document.querySelector('.board-container').appendChild(div);
+    generateBoard (x) {
+        var columns = x;
+        for(let i = 0; i <= columns; i++){
+            let col = document.createElement("div");
+            col.classList.add('pixel-col')
+            document.querySelector('.board-container').appendChild(col)
+                for(let j = 0; j <= columns; j++) {
+                    let pixel = document.createElement("div");
+                    pixel.classList.add('pixel');
+                    pixel.addEventListener('mouseover', () => {
+                        pixel.classList.add('color-pixel')
+                    });
+                    col.appendChild(pixel)
+                }
         }
     }
+
+    clearBoard(){
+        let board = document.querySelector('.board-container');
+        board.innerHTML = '';
+    }
+
 }
 
-let defaultBoard = new EtchBoard(16,16);
-defaultBoard.generateBoard(16,16);
+let DefaultBoard = new EtchBoard;
+const defaultSize = 16;
+let resetButton = document.querySelector('.reset-button')
+
+resetButton.addEventListener('click', () =>{
+    DefaultBoard.clearBoard();
+    DefaultBoard.generateBoard(defaultSize);
+});
+let generateButton = document.querySelector('.new-board-button')
+generateButton.addEventListener('click', () => {
+    let genNum = prompt('Enter a number x to generate an x by x grid')
+    DefaultBoard.generateBoard(parseInt(genNum))
+})
+DefaultBoard.generateBoard(defaultSize)
